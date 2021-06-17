fd7e9284470ff36c6066bd0d8b66f58d8c01b055
= <=>
2021-06-17 15:29:58 +0300

    Flutter Fetch Data

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:convert' show jsonDecode;
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FetchData(),
+    );
+  }
+}
+
+class FetchData extends StatefulWidget {
+  @override
+  _FetchDataState createState() => _FetchDataState();
+}
+
+class _FetchDataState extends State<FetchData> {
+  Future getUserData() async {
+    var response =
+        await http.get(Uri.https('jsonplaceholder.typicode.com', 'users'));
+    var jsonData = jsonDecode(response.body);
+    List<User> users = [];
+
+    for (var u in jsonData) {
+      User user = User(u["name"], u["email"], u["username"]);
+      users.add(user);
+    }
+    print(users.length);
+    return users;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Data'),
+      ),
+      body: Container(
+        child: Card(
+          child: FutureBuilder(
+            future: getUserData(),
+            builder: (context, snapshot) {
+              if (snapshot.data == null) {
+                return Container(
+                  child: Center(
+                    child: Text('Loading...'),
+                  ),
+                );
+              } else
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, i) {
+                      return ListTile(
+                        title: Text(snapshot.data[i].name),
+                        subtitle: Text(snapshot.data[i].userName),
+                        trailing: Text(snapshot.data[i].email),
+                      );
+                    });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class User {
+  final String name, email, userName;
+
+  User(this.name, this.email, this.userName);
+}
+
